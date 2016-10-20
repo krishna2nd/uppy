0145ae4b79443188ec51743f51b748de7043c965
Harry Hedger <hedgerh@gmail.com>
2016-10-19 23:31:30 -0400

    Progress events and endpoint setup

--- src/plugins/GoogleDrive/index.js
+++ src/plugins/GoogleDrive/index.js
@@ -109,7 +109,7 @@ export default class Google extends Plugin {
    * @return {Promise} authentication status
    */
   checkAuthentication () {
-    return fetch(`${this.opts.host}/drive/authorize`, {
+    return fetch(`${this.opts.host}/drive/auth`, {
       method: 'get',
       credentials: 'include',
       headers: {
